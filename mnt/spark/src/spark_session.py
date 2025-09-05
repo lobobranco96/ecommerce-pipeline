3878f3cfe6a90b0b596a0c82b37a3b784fae9804
lobobranco96 <rntt2511@gmail.com>
2025-09-05 21:09:06 +0000

    spark settings folder

---
+++ mnt/spark/src/spark_session.py
@@ -0,0 +1,38 @@
+import logging
+import pyspark
+from pyspark.sql import SparkSession
+
+# Configuração do logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+def create_spark_session():
+    """
+    Cria e configura uma sessão do Apache Spark.
+    
+    Returns:
+        SparkSession: Sessão Spark configurada.
+    
+    Raises:
+        Exception: Se houver falha na criação da sessão.
+    """
+    try:
+        logger.info("Iniciando a configuração da Spark Session")
+        
+        conf = (
+            pyspark.SparkConf()
+            .set("spark.master", "spark://spark-master:7077")
+        )
+        
+        spark = SparkSession.builder \
+            .appName("Minio Integration with PySpark") \
+            .config(conf=conf) \
+            .getOrCreate()
+        
+        logger.info("Spark Session criada com sucesso")
+        return spark
+    
+    except Exception as e:
+        logger.error("Erro ao criar a Spark Session", exc_info=True)
+        raise
