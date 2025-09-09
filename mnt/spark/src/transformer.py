e1a644219215c797ab82ef34c5d5086ea34ac5b2
lobobranco96 <rntt2511@gmail.com>
2025-09-09 11:25:19 +0000

    refactor transformer

--- mnt/spark/src/transformer.py
+++ mnt/spark/src/transformer.py
@@ -1,145 +1,145 @@
 from datetime import datetime
 import logging 
 
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, DateType, TimestampType
-from pyspark.sql.functions import year, month, dayofmonth, col, trim, lower
+from pyspark.sql.types import (
+    StructType, StructField, StringType, IntegerType, DoubleType, DateType, TimestampType
+)
+from pyspark.sql.functions import (
+    year, month, dayofmonth, col, trim, lower, current_timestamp
+)
 
 logger = logging.getLogger(__name__)
 
 class Transformer:
   def __init__(self, spark):
-    self.spark = spark
-    self.processed_bucket = "processed_bucket/"
+      self.spark = spark
+      self.processed_bucket = "processed_bucket/"
+      self.now = datetime.now()
 
   def orders(self, file_path: str):
+    df = self.spark.read.format("parquet").load(file_path)
 
-    order_schema = StructType([
-    StructField("order_id", StringType(), True),
-    StructField("user_id", StringType(), True),
-    StructField("product_id", StringType(), True),
-    StructField("quantity", IntegerType(), True),      
-    StructField("total_price", DoubleType(), True),
-    StructField("order_date", TimestampType(), True),
-    StructField("status", StringType(), True)
-    ])
-    df = self.spark.read.format("csv").schema(order_schema).option("header", True).load(file_path)
+    df_casted = (
+    df.withColumn("order_id", col("order_id").cast(StringType()))
+      .withColumn("user_id", col("user_id").cast(StringType()))
+      .withColumn("product_id", col("product_id").cast(StringType()))
+      .withColumn("quantity", col("quantity").cast(DoubleType()))
+      .withColumn("total_price", col("total_price").cast(DoubleType()))
+      .withColumn("order_date", col("order_date").cast(TimestampType()))
+      .withColumn("status", col("status").cast(StringType()))
+)
 
-    df_transformed = df \
-        .withColumn("order_year", year("order_date")) \
-        .withColumn("order_month", month("order_date")) \
-        .withColumn("order_day", dayofmonth("order_date")) \
-        .withColumn("status", trim(lower("status"))) \
-        .na.fill({"total_price": 0.0})  \
-        .na.drop(subset=["user_id", "total_price"])
+    df_transformed = (
+        df_casted.dropDuplicates()
+            .withColumn("order_year", year("order_date"))
+            .withColumn("order_month", month("order_date"))
+            .withColumn("order_day", dayofmonth("order_date"))
+            .withColumn("status", trim(lower("status")))
+            .na.drop(subset=["user_id", "quantity", "total_price"]) 
+            .filter(col("quantity") > 0)                            
+            .filter(col("total_price") > 0)                      
+            .filter(col("order_date") <= current_timestamp())
+    )
 
-    return df_transformed
-    # today = datetime.today()
-    # year, month, day = today.strftime("%Y"), today.strftime("%m"), today.strftime("%d")
+    file_processed_path = file_path.replace("raw", "processed")
+    try:
+      df_transformed.write \
+          .mode("overwrite") \
+          .parquet(file_processed_path)
+      logger.info(f"Data written successfully to {file_path}")
+    except Exception as e:
+        logger.error(f"Error writing the data to {file_path}: {str(e)}")
+        raise e
 
-    # key = f"orders/year={year}/month={month}/day={day}/orders.parquet"
-    # file_path = "/content/drive/MyDrive/projetos/ecommerce-pipeline/include/processed/" + key
-    # #processed_path = file_path + key
-    # try:
-    #   df.write \
-    #       .mode("overwrite") \
-    #       .parquet(file_path)
-    #   logger.info(f"Data written successfully to {file_path}")
-    # except Exception as e:
-    #     logger.error(f"Error writing the data to {file_path}: {str(e)}")
-    #     raise e
 
   def payments(self, file_path: str):
-    payment_schema = StructType([
-    StructField("payment_id", StringType(), True),
-    StructField("order_id", StringType(), True),
-    StructField("payment_method", StringType(), True),
-    StructField("amount", DoubleType(), True),
-    StructField("paid_at", TimestampType(), True)
-    ])
-    df = self.spark.read.format("csv").schema(payment_schema).option("header", True).load(file_path)
+    df = self.spark.read.format("parquet").load(file_path)
 
-    df_transformed = df \
-        .withColumn("paid_year", year("paid_at")) \
-        .withColumn("paid_month", month("paid_at")) \
-        .withColumn("paid_day", dayofmonth("paid_at")) \
-        .withColumn("payment_method", lower(col("payment_method"))) \
-        .filter(col("amount").isNotNull() & (col("amount") > 0))
-    return df_transformed
-    # today = datetime.today()
-    # year, month, day = today.strftime("%Y"), today.strftime("%m"), today.strftime("%d")
+    df_casted = (
+        df.withColumn("payment_id", col("payment_id").cast(StringType()))
+          .withColumn("order_id", col("order_id").cast(StringType()))
+          .withColumn("payment_method", col("payment_method").cast(StringType()))
+          .withColumn("amount", col("amount").cast(DoubleType()))
+          .withColumn("paid_at", col("paid_at").cast(TimestampType()))
+    )
 
-    # key = f"payments/year={year}/month={month}/day={day}/payments.parquet"
-    # file_path = "/content/drive/MyDrive/projetos/ecommerce-pipeline/include/processed/" + key
-    # #processed_path = file_path + key
-    # try:
-    #   df.write \
-    #       .mode("overwrite") \
-    #       .parquet(file_path)
-    #   logger.info(f"Data written successfully to {file_path}")
-    # except Exception as e:
-    #     logger.error(f"Error writing the data to {file_path}: {str(e)}")
-    #     raise e
-  
+    df_transformed = (
+        df_casted.dropDuplicates()
+            .withColumn("paid_year", year("paid_at"))
+            .withColumn("paid_month", month("paid_at"))
+            .withColumn("paid_day", dayofmonth("paid_at"))
+            .withColumn("payment_method", lower(col("payment_method")))
+            .filter(col("amount").isNotNull() & (col("amount") > 0))  
+            .filter(col("paid_at").isNotNull() & (col("paid_at") <= current_timestamp()))
+    )
+
+    file_processed_path = file_path.replace("raw", "processed")
+    try:
+      df_transformed.write \
+          .mode("overwrite") \
+          .parquet(file_processed_path)
+      logger.info(f"Data written successfully to {file_path}")
+    except Exception as e:
+        logger.error(f"Error writing the data to {file_path}: {str(e)}")
+        raise e
+
   def products(self, file_path: str):
-    product_schema = StructType([
-    StructField("product_id", StringType(), True),
-    StructField("name", StringType(), True),
-    StructField("category", StringType(), True),
-    StructField("price", DoubleType(), True),
-    StructField("stock", IntegerType(), True)
-    ])
-    df = self.spark.read.format("csv").schema(product_schema).option("header", True).load(file_path)
+    df = self.spark.read.format("parquet").load(file_path)
 
-    df_transformed = df \
-        .withColumn("category", lower(trim(col("category")))) \
-        .withColumn("name", trim(col("name"))) \
-        .filter(col("price").isNotNull() & (col("price") > 0)) \
-        .filter(col("stock").isNotNull() & (col("stock") >= 0))
-    return df_transformed
-    # today = datetime.today()
-    # year, month, day = today.strftime("%Y"), today.strftime("%m"), today.strftime("%d")
+    df_casted = (
+        df.withColumn("product_id", col("product_id").cast(StringType()))
+          .withColumn("name", col("name").cast(StringType()))
+          .withColumn("category", col("category").cast(StringType()))
+          .withColumn("price", col("price").cast(DoubleType()))
+          .withColumn("stock", col("stock").cast(IntegerType()))
+    )
 
-    # key = f"products/year={year}/month={month}/day={day}/products.parquet"
-    # file_path = "/content/drive/MyDrive/projetos/ecommerce-pipeline/include/processed/" + key
-    # #processed_path = file_path + key
-    # try:
-    #   df.write \
-    #       .mode("overwrite") \
-    #       .parquet(file_path)
-    #   logger.info(f"Data written successfully to {file_path}")
-    # except Exception as e:
-    #     logger.error(f"Error writing the data to {file_path}: {str(e)}")
-    #     raise e
-  
+    df_transformed = (
+        df_casted.dropDuplicates()
+            .withColumn("category", lower(trim(col("category"))))
+            .withColumn("name", trim(col("name")))
+            .filter(col("price").isNotNull() & (col("price") > 0))  
+            .filter(col("stock").isNotNull() & (col("stock") >= 0))
+    )
+
+    file_processed_path = file_path.replace("raw", "processed")
+    try:
+      df_transformed.write \
+          .mode("overwrite") \
+          .parquet(file_processed_path)
+      logger.info(f"Data written successfully to {file_path}")
+    except Exception as e:
+        logger.error(f"Error writing the data to {file_path}: {str(e)}")
+        raise e
+
   def users(self, file_path: str):
-    user_schema = StructType([
-    StructField("user_id", StringType(), True),
-    StructField("name", StringType(), True),
-    StructField("email", StringType(), True),
-    StructField("signup_date", DateType(), True),
-    StructField("city", StringType(), True),
-    StructField("state", StringType(), True)
-    ])
-    df = self.spark.read.format("csv").schema(user_schema).option("header", True).load(file_path)
-    df_transformed = df \
-        .filter(col("user_id").isNotNull()) \
-        .withColumn("name", trim(col("name"))) \
-        .withColumn("email", lower(trim(col("email")))) \
-        .withColumn("city", trim(col("city"))) \
-        .withColumn("state", trim(col("state")))
+    df = self.spark.read.format("parquet").load(file_path)
 
-    return df_transformed
-    # today = datetime.today()
-    # year, month, day = today.strftime("%Y"), today.strftime("%m"), today.strftime("%d")
+    df_casted = (
+        df.withColumn("user_id", col("user_id").cast(StringType()))
+          .withColumn("name", col("name").cast(StringType()))
+          .withColumn("email", col("email").cast(StringType()))
+          .withColumn("signup_date", col("signup_date").cast(DateType()))
+          .withColumn("city", col("city").cast(StringType()))
+          .withColumn("state", col("state").cast(StringType()))
+    )
 
-    # key = f"users/year={year}/month={month}/day={day}/users.parquet"
-    # file_path = "/content/drive/MyDrive/projetos/ecommerce-pipeline/include/processed/" + key
-    # #processed_path = file_path + key
-    # try:
-    #   df.write \
-    #       .mode("overwrite") \
-    #       .parquet(file_path)
-    #   logger.info(f"Data written successfully to {file_path}")
-    # except Exception as e:
-    #     logger.error(f"Error writing the data to {file_path}: {str(e)}")
-    #     raise e
+    df_transformed = (
+        df_casted.dropDuplicates()
+            .filter(col("user_id").isNotNull())
+            .filter(col("email").isNotNull())
+            .withColumn("name", trim(col("name")))
+            .withColumn("email", lower(trim(col("email"))))
+            .withColumn("city", trim(col("city")))
+            .withColumn("state", trim(col("state")))
+    )
+
+    file_processed_path = file_path.replace("raw", "processed")
+    try:
+      df_transformed.write \
+          .mode("overwrite") \
+          .parquet(file_processed_path)
+      logger.info(f"Data written successfully to {file_path}")
+    except Exception as e:
+        logger.error(f"Error writing the data to {file_path}: {str(e)}")
+        raise e
