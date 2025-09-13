450a4b958b8e67fbce1ee408857adaf3e4c1cd24
lobobranco96 <rntt2511@gmail.com>
2025-09-13 14:01:07 +0000

    fix unnecessary import

--- mnt/spark/utils/load_postgres.py
+++ mnt/spark/utils/load_postgres.py
@@ -1,7 +1,5 @@
 import os
 
-from IPython.utils import strdispatch
-
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
